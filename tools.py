091ba278a928299ae164d8c4a3597a118c526e0d
Denys Lazarenko <denys.lazarenko@capgemini.com>
2016-02-03 00:12:00 +0100

    Refactoring: read_lines => read_local_file, parse_fasta_file => read_fasta_file, parse_fasta_lines

--- tools.py
+++ tools.py
@@ -3,7 +3,16 @@ import urllib.request
 
 
 
-def read_lines(path):
+def read_local_file(path):
+    """
+    Read a local file.
+    
+    Args:
+        + path (str): path to a local file
+    
+    Returns:
+        (list of str): lines representing file's content
+    """
     lines = []
     with open(path, 'r') as in_file:
         for line in in_file:
@@ -19,13 +28,13 @@ def read_fasta_file(path):
     Args:
         + path (str): path to a FASTA file, either local or URL
     """
-    if path.starswith('http://'):
+    if path.startswith('http://'):
         req = urllib.request.Request(path, headers={'User-Agent': 'Mozilla/5.0'})
         with urllib.request.urlopen(req) as response:
             html = str(response.read())
         #lines = 
-    
-    lines = read_lines(path)
+    else:
+        lines = read_local_file(path)
     return parse_fasta_lines(lines)
 
 
@@ -67,63 +76,6 @@ def parse_fasta_lines(lines):
             key:   taxon name,
             value: taxon genetic string
     """
-    d = {}
-    current_taxon = None
-    for line in lines:
-        # If it is a taxon title line, create an entry in the dictionary and set this taxon to be the current key
-        if line.startswith('>'):
-            taxon_name = line.strip('>')
-            if taxon_name in d:
-                raise Exception("Taxon {0} found more than once in a file!".format(taxon_name))
-            else:
-                d[taxon_name] = ''
-                current_taxon = taxon_name
-        # If it is a line containing part of a genetic string, add this line to the current taxon's entry (concatenate)
-        else:
-            d[current_taxon] += line
-    return d
-
-
-
-    
-# TODO deprecated
-def parse_fasta_file(path):
-    """
-    A file in FASTA format (.fas, .fasta) applies the following sample notation to store genetic strings:
-
-    Example:
-
-    >Taxon1
-    CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
-    TCCCACTAATAATTCTGAGG
-    >Taxon2
-    CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
-    ATATCCATTTGTCAGCAGACACGC
-    >Taxon3
-    CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
-    TGGGAACCTGCGGGCAGTAGGTGGAAT
-
-    Every string in a FASTA file begins with a single-line that contains the symbol '>' 
-    along with some labeling information about the string. 
-    The word following the '>' symbol is the identifier of the sequence, 
-    and the rest of the line is its description (both are optional). 
-    There should be no space between the ">" and the first letter of the identifier.
-
-    All subsequent lines contain the string itself; 
-    it is recommended that all lines of text are shorter than 80 symbols. 
-    The string ends when another line starting with '>' appears, 
-    indicating the start of another sequence (or if the end of the file is reached).
-    
-    
-    Args:
-        + path (str): path to an input file
-        
-    Returns:
-        + dictionary {str: str}:
-            key:   taxon name,
-            value: taxon genetic string
-    """
-    lines = read_lines(path)
     d = {}
     current_taxon = None
     for line in lines:
